2e2324dd06e09ac815798cd9e2b4277278120498
felix2000jp <felix2000jp@gmail.com>
2025-04-06 11:13:28 +0100

    add appuser and security

--- src/test/java/dev/felix2000jp/springboottemplate/ApplicationTests.java
+++ src/test/java/dev/felix2000jp/springboottemplate/ApplicationTests.java
@@ -2,12 +2,8 @@ package dev.felix2000jp.springboottemplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.springframework.modulith.core.ApplicationModules;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Import(TestcontainersConfiguration.class)
