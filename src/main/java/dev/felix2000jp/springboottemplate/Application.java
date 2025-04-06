a1b34ff8e37332d32c2d63a18c6a3564ea4f609b
felix2000jp <felix2000jp@gmail.com>
2025-04-06 11:48:24 +0100

    add appuser and security

--- src/main/java/dev/felix2000jp/springboottemplate/Application.java
+++ src/main/java/dev/felix2000jp/springboottemplate/Application.java
@@ -1,9 +1,9 @@
 package dev.felix2000jp.springboottemplate;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulith;
 
-@SpringBootApplication
+@Modulith(sharedModules = {"shared"})
 public class Application {
 
     public static void main(String[] args) {
