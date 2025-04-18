12c51c1197b76509699df1ca0d4733859361ec0d
felix2000jp <felix2000jp@gmail.com>
2025-04-18 18:41:26 +0100

    add note controller tests

---
+++ src/test/java/dev/felix2000jp/springboottemplate/notes/infrastructure/api/NoteControllerIntegrationTest.java
@@ -0,0 +1,138 @@
+package dev.felix2000jp.springboottemplate.notes.infrastructure.api;
+
+import dev.felix2000jp.springboottemplate.TestcontainersConfiguration;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.CreateNoteDto;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.NoteDto;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.NoteListDto;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.UpdateNoteDto;
+import dev.felix2000jp.springboottemplate.notes.domain.Note;
+import dev.felix2000jp.springboottemplate.notes.domain.NoteRepository;
+import dev.felix2000jp.springboottemplate.system.security.SecurityScope;
+import dev.felix2000jp.springboottemplate.system.security.SecurityService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationModuleTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import({TestcontainersConfiguration.class})
+class NoteControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    @Autowired
+    private SecurityService securityService;
+    @Autowired
+    private NoteRepository noteRepository;
+
+    private Note note;
+    private HttpHeaders headers;
+
+    @BeforeEach
+    void setUp() {
+        noteRepository.deleteAll();
+
+        note = Note.from(UUID.randomUUID(), UUID.randomUUID(), "title", "content");
+        noteRepository.save(note);
+
+        var token = securityService.generateToken(
+                note.getId().appuserIdValue(),
+                "username",
+                List.of(SecurityScope.APPLICATION)
+        );
+
+        headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+    }
+
+    @Test
+    void get_return_notes() {
+        var findNoteBydIdEntity = testRestTemplate.exchange(
+                "/api/notes",
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                NoteListDto.class
+        );
+
+        assertThat(findNoteBydIdEntity.getStatusCode().value()).isEqualTo(200);
+        assertThat(findNoteBydIdEntity.getBody()).isNotNull();
+        assertThat(findNoteBydIdEntity.getBody().notes()).hasSize(1);
+    }
+
+    @Test
+    void getByNoteIdValue_then_return_note() {
+        var findNoteBydIdEntity = testRestTemplate.exchange(
+                "/api/notes/{id}",
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                NoteDto.class,
+                note.getId().noteIdValue()
+        );
+
+        assertThat(findNoteBydIdEntity.getStatusCode().value()).isEqualTo(200);
+        assertThat(findNoteBydIdEntity.getBody()).isNotNull();
+        assertThat(findNoteBydIdEntity.getBody().title()).isEqualTo(note.getTitle().value());
+        assertThat(findNoteBydIdEntity.getBody().content()).isEqualTo(note.getContent().value());
+    }
+
+    @Test
+    void create_then_create_note() {
+        var createNoteEntity = testRestTemplate.exchange(
+                "/api/notes",
+                HttpMethod.POST,
+                new HttpEntity<>(new CreateNoteDto("title", "content"), headers),
+                Void.class
+        );
+
+        assertThat(createNoteEntity.getStatusCode().value()).isEqualTo(201);
+
+        var userNotes = noteRepository.findAllByIdAppuserIdValue(note.getId().appuserIdValue());
+        assertThat(userNotes.size()).isEqualTo(2);
+    }
+
+    @Test
+    void update_then_update_note() {
+        var updateNoteEntity = testRestTemplate.exchange(
+                "/api/notes/{id}",
+                HttpMethod.PUT,
+                new HttpEntity<>(new UpdateNoteDto("new title", "new content"), headers),
+                Void.class,
+                note.getId().noteIdValue()
+        );
+
+        assertThat(updateNoteEntity.getStatusCode().value()).isEqualTo(204);
+
+        var updatedNote = noteRepository.findById(note.getId());
+        assertThat(updatedNote).isPresent();
+        assertThat(updatedNote.get().getTitle().value()).isEqualTo("new title");
+        assertThat(updatedNote.get().getContent().value()).isEqualTo("new content");
+    }
+
+    @Test
+    void deleteByNoteIdValue_then_delete_note() {
+        var deleteNoteEntity = testRestTemplate.exchange(
+                "/api/notes/{id}",
+                HttpMethod.DELETE,
+                new HttpEntity<>(headers),
+                Void.class,
+                note.getId().noteIdValue()
+        );
+
+        assertThat(deleteNoteEntity.getStatusCode().value()).isEqualTo(204);
+
+        var deletedNote = noteRepository.findById(note.getId());
+        assertThat(deletedNote).isNotPresent();
+    }
+
+}
---
+++ src/test/java/dev/felix2000jp/springboottemplate/notes/infrastructure/api/NoteControllerTest.java
@@ -0,0 +1,224 @@
+package dev.felix2000jp.springboottemplate.notes.infrastructure.api;
+
+import dev.felix2000jp.springboottemplate.notes.application.NoteService;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.CreateNoteDto;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.NoteDto;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.NoteListDto;
+import dev.felix2000jp.springboottemplate.notes.application.dtos.UpdateNoteDto;
+import dev.felix2000jp.springboottemplate.notes.domain.exceptions.NoteNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(excludeAutoConfiguration = SecurityAutoConfiguration.class, controllers = NoteController.class)
+class NoteControllerTest {
+
+    @MockitoBean
+    private NoteService noteService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void get_then_return_200_and_notes() throws Exception {
+        var noteDto = new NoteDto(UUID.randomUUID(), "title", "content");
+        var noteListDto = new NoteListDto(1, List.of(noteDto));
+
+        var expectedResponse = String.format("""
+                {
+                    "notes": [{ "id": "%s", "title": "%s", "content": "%s" }]
+                }
+                """, noteDto.id(), noteDto.title(), noteDto.content());
+
+        when(noteService.get()).thenReturn(noteListDto);
+
+        var request = get("/api/notes");
+        mockMvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+    }
+
+    @Test
+    void getByNoteIdValue_given_id_then_return_200_and_note() throws Exception {
+        var noteDto = new NoteDto(UUID.randomUUID(), "title", "content");
+        var expectedResponse = String.format("""
+                {
+                    "id": "%s",
+                    "title": "%s",
+                    "content": "%s"
+                }
+                """, noteDto.id(), noteDto.title(), noteDto.content());
+
+        when(noteService.getByNoteIdValue(noteDto.id())).thenReturn(noteDto);
+
+        var request = get("/api/notes/" + noteDto.id());
+        mockMvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+    }
+
+    @Test
+    void getByNoteIdValue_given_not_found_id_then_return_404() throws Exception {
+        var exception = new NoteNotFoundException();
+        when(noteService.getByNoteIdValue(any())).thenThrow(exception);
+
+        var request = get("/api/notes/" + UUID.randomUUID());
+        mockMvc
+                .perform(request)
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.detail").value(exception.getMessage()))
+                .andExpect(jsonPath("$.status").value(404));
+    }
+
+    @Test
+    void create_then_return_201_and_location_header() throws Exception {
+        var noteDto = new NoteDto(UUID.randomUUID(), "title", "content");
+        var createNoteDto = new CreateNoteDto(noteDto.title(), noteDto.content());
+
+        var body = String.format("""
+                { "title": "%s", "content": "%s" }
+                """, createNoteDto.title(), createNoteDto.content());
+        var expectedResponse = String.format("""
+                { "id": "%s", "title": "%s", "content": "%s" }
+                """, noteDto.id(), noteDto.title(), noteDto.content());
+
+        var request = post("/api/notes").contentType(MediaType.APPLICATION_JSON).content(body);
+        mockMvc
+                .perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(header().string("LOCATION", "/api/notes"));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void create_given_invalid_body_then_return_404(String body) throws Exception {
+        var request = post("/api/notes").contentType(MediaType.APPLICATION_JSON).content(body);
+        mockMvc
+                .perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title").value("Bad Request"))
+                .andExpect(jsonPath("$.status").value(400));
+    }
+
+    private static Stream<Arguments> create_given_invalid_body_then_return_404() {
+        return Stream.of(
+                arguments(""),
+                arguments("{}"),
+                arguments("{ 'title': 'title' }"),
+                arguments("{ 'content': 'content' }"),
+                arguments("{ 'title': null, 'content': 'content' }"),
+                arguments("{ 'title': '', 'content': 'content' }"),
+                arguments("{ 'title': ' ', 'content': 'content' }"),
+                arguments("{ 'title': 'title', 'content': null }"),
+                arguments("{ 'title': 'title', 'content': '' }"),
+                arguments("{ 'title': 'title', 'content': ' ' }")
+        );
+    }
+
+    @Test
+    void update_then_return_204() throws Exception {
+        var noteDto = new NoteDto(UUID.randomUUID(), "title", "content");
+        var updateNoteDto = new UpdateNoteDto(noteDto.title(), noteDto.content());
+
+        var body = String.format("""
+                { "title": "%s", "content": "%s" }
+                """, updateNoteDto.title(), updateNoteDto.content());
+
+        var request = put("/api/notes/" + noteDto.id()).contentType(MediaType.APPLICATION_JSON).content(body);
+        mockMvc
+                .perform(request)
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void update_given_not_found_id_then_return_404() throws Exception {
+        var id = UUID.randomUUID();
+        var updateNoteDto = new UpdateNoteDto("title", "content");
+
+        var body = String.format("""
+                { "title": "%s", "content": "%s" }
+                """, updateNoteDto.title(), updateNoteDto.content());
+
+        var exception = new NoteNotFoundException();
+        doThrow(exception).when(noteService).update(id, updateNoteDto);
+
+        var request = put("/api/notes/" + id).contentType(MediaType.APPLICATION_JSON).content(body);
+        mockMvc
+                .perform(request)
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.detail").value(exception.getMessage()))
+                .andExpect(jsonPath("$.status").value(404));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void update_given_invalid_body_then_return_400(String body) throws Exception {
+        var request = put("/api/notes/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON).content(body);
+        mockMvc
+                .perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title").value("Bad Request"))
+                .andExpect(jsonPath("$.status").value(400));
+    }
+
+    private static Stream<Arguments> update_given_invalid_body_then_return_400() {
+        return Stream.of(
+                arguments(""),
+                arguments("{}"),
+                arguments("{ 'title': 'title' }"),
+                arguments("{ 'content': 'content' }"),
+                arguments("{ 'title': null, 'content': 'content' }"),
+                arguments("{ 'title': '', 'content': 'content' }"),
+                arguments("{ 'title': ' ', 'content': 'content' }"),
+                arguments("{ 'title': 'title', 'content': null }"),
+                arguments("{ 'title': 'title', 'content': '' }"),
+                arguments("{ 'title': 'title', 'content': ' ' }")
+        );
+    }
+
+    @Test
+    void deleteByNoteIdValue_given_id_then_return_204() throws Exception {
+        var request = delete("/api/notes/" + UUID.randomUUID());
+        mockMvc
+                .perform(request)
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteByNoteIdValue_given_not_found_id_then_return_404() throws Exception {
+        var id = UUID.randomUUID();
+
+        var exception = new NoteNotFoundException();
+        doThrow(exception).when(noteService).deleteByNoteIdValue(id);
+
+        var request = delete("/api/notes/" + id);
+        mockMvc
+                .perform(request)
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.detail").value(exception.getMessage()))
+                .andExpect(jsonPath("$.status").value(404));
+    }
+
+}
