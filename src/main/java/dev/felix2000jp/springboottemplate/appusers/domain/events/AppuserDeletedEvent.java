824eae4fd07ea71f5cfeb5877e208d960aac4ee7
felix2000jp <felix2000jp@gmail.com>
2025-04-18 15:46:39 +0100

    make AppuserDeletedEvent a named interface

--- src/main/java/dev/felix2000jp/springboottemplate/appusers/domain/events/AppuserDeletedEvent.java
+++ src/main/java/dev/felix2000jp/springboottemplate/appusers/domain/events/AppuserDeletedEvent.java
@@ -1,8 +1,10 @@
 package dev.felix2000jp.springboottemplate.appusers.domain.events;
 
 import org.jmolecules.event.types.DomainEvent;
+import org.springframework.modulith.NamedInterface;
 
 import java.util.UUID;
 
+@NamedInterface
 public record AppuserDeletedEvent(UUID appuserId) implements DomainEvent {
 }
