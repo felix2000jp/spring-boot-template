3577e04003fece2cb1eb853e7a08ac4b33bcec1e
felix2000jp <felix2000jp@gmail.com>
2025-08-26 16:08:53 +0100

    add system configurations package

--- src/main/java/dev/felix2000jp/springboottemplate/system/configurations/SecurityConfiguration.java
+++ src/main/java/dev/felix2000jp/springboottemplate/system/configurations/SecurityConfiguration.java
@@ -1,8 +1,9 @@
-package dev.felix2000jp.springboottemplate.system.security;
+package dev.felix2000jp.springboottemplate.system.configurations;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import dev.felix2000jp.springboottemplate.system.security.SecurityScope;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
