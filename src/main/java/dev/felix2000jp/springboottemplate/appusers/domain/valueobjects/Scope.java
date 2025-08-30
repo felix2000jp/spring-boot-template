a5584a16d5dab78232d83f42321475e1f003d5c1
João Félix <61743088+felix2000jp@users.noreply.github.com>
2025-08-30 17:32:43 +0100

    refactor: prefer Attribute override (#45)

--- src/main/java/dev/felix2000jp/springboottemplate/appusers/domain/valueobjects/Scope.java
+++ src/main/java/dev/felix2000jp/springboottemplate/appusers/domain/valueobjects/Scope.java
@@ -1,15 +1,11 @@
 package dev.felix2000jp.springboottemplate.appusers.domain.valueobjects;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import org.jmolecules.ddd.types.ValueObject;
 import org.springframework.util.Assert;
 
 @Embeddable
-public record Scope(
-        @Column(name = "scope")
-        String value
-) implements ValueObject {
+public record Scope(String value) implements ValueObject {
 
     public Scope {
         Assert.notNull(value, "value cannot be null");
--- src/main/java/dev/felix2000jp/springboottemplate/appusers/domain/valueobjects/Username.java
+++ src/main/java/dev/felix2000jp/springboottemplate/appusers/domain/valueobjects/Username.java
@@ -1,15 +1,11 @@
 package dev.felix2000jp.springboottemplate.appusers.domain.valueobjects;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import org.jmolecules.ddd.types.ValueObject;
 import org.springframework.util.Assert;
 
 @Embeddable
-public record Username(
-        @Column(name = "username")
-        String value
-) implements ValueObject {
+public record Username(String value) implements ValueObject {
 
     public Username {
         Assert.notNull(value, "value cannot be null");
