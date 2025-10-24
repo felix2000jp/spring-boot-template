a58dca216ba45ad8a04347ea1f22c1bdaf819e18
felix2000jp <felix2000jp@gmail.com>
2025-10-24 23:35:43 +0100

    fix tests and application visibility

--- src/main/java/dev/felix2000jp/springboottemplate/Application.java
+++ src/main/java/dev/felix2000jp/springboottemplate/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.modulith.Modulith;
 
 @Modulith(systemName = "Spring Boot Template", sharedModules = "system")
-class Application {
+public class Application {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
